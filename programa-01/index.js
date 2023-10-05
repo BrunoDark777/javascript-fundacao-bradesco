e13a169f114ec1ce1ee1b950c97e7b8328f65e7b
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-10-05 04:43:09 -0300

    JavaScript aula 1

---
+++ programa-01/index.js
@@ -0,0 +1,16 @@
+// Objetivo : captura o nome do usurio e apresentar uma saudação na tela.
+
+// Definição de variaveis
+let js_nome = document.getElementById('nome')
+
+// Entrada de dados
+js_nome = window.prompt('Entre com seu nome: ', ' Digite seu nome nessa caixa')
+
+// Saida de dados
+document.write('Ola ' + js_nome + ' seja bem vindo!')
+// Mensagem de saudação personalizada.
+
+
+
+
+
