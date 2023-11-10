0621cec179288f536c355d225266a8ee2d5df9fa
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-11-10 02:42:14 -0300

    do while

---
+++ programa-12/index.js
@@ -0,0 +1,21 @@
+/* Objetivo: Calcular o salário líquido de um funcionário que trabalha por hora (horista). */
+// - Para desenvolver o cálculo é necessário ter as seguintes informações:
+     // - Quantidade de horas trabalhadas no mês (js_qht).
+    // - Valor da hora de trabalho (js_vht).
+   // - Percentual de desconto do INSS (js_inss)
+// - Informar a classificação salárial.
+   // Objetivo/Metodo utilizado:
+    // Entrada: window.prompt
+    // Saída: document.write
+
+//  Definição de variaveis:
+
+let js_qht // var de entrada - Quantidade de Horas Trabalhadas
+let js_vht // var de entrada - Valor da Hora Trabalhada
+let js_inss // var de entrada - Percentual de Desconto INSS
+let js_tdes // var de saída - Total de Descontos
+let js_sb // var de saída - Valor do Salário Bruto
+let js_sl // var de saída - Valor do Salário Liquido
+let js_resposta = true 
+
+// Entradaa de Dados:
